7e0421b8cf1fc8ba7b7f8dfe041c5fb5a86529e9
FeHari <alexandregomesap202@gmail.com>
2024-10-06 16:26:37 -0300

    ArceusScript.lua

--- ArceusScript.lua
+++ ArceusScript.lua
@@ -1,30 +1,56 @@
 local ArceusUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/ArceusX-V3-Scripts/main/ArceusUI.lua"))()
 
--- Set up the UI title
-ArceusUI:SetTitle("Arceus X <font color='rgb(255, 0, 0)'>|</font> MyScript")
+-- Título Do Script
+ArceusUI:SetTitle("FeHari Hub <font color='rgb(255, 0, 0)'>|</font> Lendas Da Velocidade ⚡")
 
--- Set up the script logo
+-- Logo Do Script
 ArceusUI:SetLogo("MyLogo.png", "https://mywebsite/myimage.png")
 
--- Adding a button
+-- Variável para controlar o estado do farming
+local isFarming = false
+
+-- Função de farming
+local function HoopFarm()
+    local Chr = game.Players.LocalPlayer.Character
+    if Chr and Chr.Parent and Chr:FindFirstChild("Head") then
+        for _, v in next, game:GetService("Workspace").Hoops:GetDescendants() do
+            if v.Name == "TouchInterest" and v.Parent then
+                firetouchinterest(Chr:WaitForChild("Head", 5), v.Parent, 0)
+                task.wait()
+                firetouchinterest(Chr:WaitForChild("Head", 5), v.Parent, 1)
+            end
+        end
+    end
+end
+
+-- Botão
 local myButton = ArceusUI:AddButton("Click Me!", function(...)
     print("Button was pressed!")
 end)
 
--- Adding a toggle
-local myToggle = ArceusUI:AddToggle("Enable Feature", function(myStatus, ...)
-    print("Toggle status:", myStatus)
-end, false) -- Initial status set to false
+-- Toggle
+local myToggle = ArceusUI:AddToggle("Aros V1", function(myStatus, ...)
+    isFarming = myStatus
+    print("Status do Toggle:", myStatus)
 
--- Adding a combo box
+    -- Iniciar ou parar o farming com base no status do toggle
+    if isFarming then
+        while isFarming do
+            HoopFarm()
+            task.wait(0.5) -- Ajuste o atraso conforme necessário
+        end
+    end
+end, false) -- Status inicial definido como falso
+
+-- Combo box
 local myCombo = ArceusUI:AddComboBox("Choose Option", {"Option 1", "Option 2", "Option 3"}, function(myChoice, ...)
     print("Combo selection:", myChoice)
 end)
 
--- Adding a numeric up-down
+-- Up-Down
 local myUpDown = ArceusUI:AddUpDown("Select Number", function(myNumericValue, ...)
     print("UpDown value:", myNumericValue)
 end, 1, 1, 1, 10) -- Starts at 1, increment by 1, min 1, max 10
 
--- Start the UI
+-- Ui Arceus X
 ArceusUI:Start()
